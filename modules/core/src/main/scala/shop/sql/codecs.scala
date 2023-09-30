c14f4e92d68f892abf251c59db4b5faeccdace79
Md Saif Husain <mdhusain02@gmail.com>
2023-09-30 21:23:16 +0530

    feat: categories interpreter for postgres

--- modules/core/src/main/scala/shop/sql/codecs.scala
+++ modules/core/src/main/scala/shop/sql/codecs.scala
@@ -1,5 +1,8 @@
 package shop.sql
+
 import shop.domain.brand._
+import shop.domain.category._
+
 
 import skunk.Codec
 import skunk.codec.all._
@@ -7,4 +10,6 @@ import skunk.codec.all._
 object codecs {
     val brandId: Codec[BrandId] = uuid.imap[BrandId](BrandId(_))(_.value)
     val brandName: Codec[BrandName] = varchar.imap[BrandName](BrandName(_))(_.value)
+    val categoryId: Codec[CategoryId] = uuid.imap[CategoryId](CategoryId(_))(_.value)
+    val categoryName: Codec[CategoryName] = varchar.imap[CategoryName](CategoryName(_))(_.value)
 }
