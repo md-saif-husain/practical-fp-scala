e1743670ec24c5bca8ebe9b51df4a682e9abf146
Md Saif Husain <mdhusain02@gmail.com>
2023-10-01 12:04:07 +0530

    feat: order interpreter for postgres

--- modules/core/src/main/scala/shop/services/Orders.scala
+++ modules/core/src/main/scala/shop/services/Orders.scala
@@ -1,11 +1,20 @@
 package shop.services
 
 import shop.domain.order._
+import shop.domain.item._
 import shop.domain.auth.UserId
-import shop.domain.cart.CartItem
+import shop.domain.cart._
+import shop.sql.codecs._
 
 import squants.market.Money
 import cats.data.NonEmptyList
+import cats.syntax.all._
+import cats.effect._
+import skunk._
+import skunk.implicits._
+import skunk.circe.codec.all._
+import shop.effects.GenUUID
+import shop.domain.ID
 
 trait Orders[F[_]] {
   def get(userId: UserId, orderId: OrderId): F[Option[Order]]
@@ -16,4 +25,82 @@ trait Orders[F[_]] {
       items: NonEmptyList[CartItem],
       total: Money
   ): F[OrderId]
+}
+
+object Orders {
+  def make[F[_]: Concurrent: GenUUID](
+      postgres: Resource[F, Session[F]]
+  ): Orders[F] =
+    new Orders[F] {
+      import OrdersSQL._
+
+      override def get(userId: UserId, orderId: OrderId): F[Option[Order]] =
+        postgres.use { session: Session[F] =>
+          session.prepare(selectByUserIdAndOrderId).use { q =>
+            q.option(userId ~ orderId)
+          }
+        }
+
+      override def findBy(userId: UserId): F[List[Order]] =
+        postgres.use { session: Session[F] =>
+          session.prepare(selectByUserId).use { q =>
+            q.stream(userId, 1024).compile.toList
+          }
+        }
+
+      override def create(
+          userId: UserId,
+          paymentId: PaymentId,
+          items: NonEmptyList[CartItem],
+          total: Money
+      ): F[OrderId] = postgres.use { session: Session[F] =>
+        session.prepare(insertOrder).use { cmd =>
+          ID.make[F, OrderId].flatMap { id =>
+            val itMap = items.toList.map(x => x.item.uuid -> x.quantity).toMap
+            val order = Order(id, paymentId, itMap, total)
+            cmd.execute(userId ~ order).as(id)
+          }
+        }
+      }
+    }
+}
+
+private object OrdersSQL {
+  val decoder: Decoder[Order] =
+    (
+      orderId ~ userId ~ paymentId ~
+        jsonb[Map[ItemId, Quantity]] ~ money
+    ).map {
+      case o ~ _ ~ p ~ i ~ t =>
+        Order(o, p, i, t)
+    }
+
+  val encoder: Encoder[UserId ~ Order] =
+    (
+      orderId ~ userId ~ paymentId ~
+        jsonb[Map[ItemId, Quantity]] ~ money
+    ).contramap {
+      case id ~ o =>
+        o.id ~ id ~ o.paymentId ~ o.items ~ o.total
+    }
+
+  val selectByUserId: Query[UserId, Order] =
+    sql"""
+      SELECT * FROM orders
+      WHERE user_id = $userId
+    """.query(decoder)
+
+  val selectByUserIdAndOrderId: Query[UserId ~ OrderId, Order] =
+    sql"""
+      SELECT * FROM orders
+      WHERE user_id = $userId
+      AND uuid = $orderId
+    """.query(decoder)
+
+  val insertOrder: Command[UserId ~ Order] =
+    sql"""
+      INSERT INTO orders
+      VALUES ($encoder)
+    """.command
+
 }
--- modules/core/src/main/scala/shop/sql/codecs.scala
+++ modules/core/src/main/scala/shop/sql/codecs.scala
@@ -3,6 +3,8 @@ package shop.sql
 import shop.domain.brand._
 import shop.domain.category._
 import shop.domain.item._
+import shop.domain.order._
+import shop.domain.auth._
 
 import skunk.Codec
 import skunk.codec.all._
@@ -18,6 +20,11 @@ object codecs {
   val itemId: Codec[ItemId]            = uuid.imap[ItemId](ItemId(_))(_.value)
   val itemName: Codec[ItemName]        = varchar.imap[ItemName](ItemName(_))(_.value)
   val itemDesc: Codec[ItemDescription] = varchar.imap[ItemDescription](ItemDescription(_))(_.value)
+
+  val orderId: Codec[OrderId]     = uuid.imap[OrderId](OrderId(_))(_.value)
+  val paymentId: Codec[PaymentId] = uuid.imap[PaymentId](PaymentId(_))(_.value)
+
+  val userId: Codec[UserId]     = uuid.imap[UserId](UserId(_))(_.value)
 
   val money: Codec[Money] = numeric.imap[Money](USD(_))(_.amount)
 }
