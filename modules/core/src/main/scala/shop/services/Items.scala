f164c296f3cfaa9954daade291e2c66a7b608a09
Md Saif Husain <mdhusain02@gmail.com>
2023-10-01 11:10:51 +0530

    feat: Items interpreter for postgres

--- modules/core/src/main/scala/shop/services/Items.scala
+++ modules/core/src/main/scala/shop/services/Items.scala
@@ -1,7 +1,16 @@
 package shop.services
 
 import shop.domain.item._
-import shop.domain.brand.BrandName
+import shop.domain.brand._
+import shop.domain.category._
+import shop.sql.codecs._
+import shop.effects._
+
+import skunk._
+import skunk.implicits._
+import cats.syntax.all._
+import cats.effect._
+import shop.domain.ID
 
 trait Items[F[_]] {
   def findAll: F[List[Item]]
@@ -9,4 +18,102 @@ trait Items[F[_]] {
   def findById(itemId: ItemId): F[Option[Item]]
   def create(item: CreateItem): F[ItemId]
   def update(item: UpdateItem): F[Unit]
+}
+
+object Items {
+  def make[F[_]: Concurrent: GenUUID](
+      postgres: Resource[F, Session[F]]
+  ): Items[F] =
+    new Items[F] {
+      import ItemSQL._
+
+      def findAll: F[List[Item]] = postgres.use(_.execute(selectAll))
+
+      def findBy(brand: BrandName): F[List[Item]] = postgres.use { session: Session[F] =>
+        session.prepare(selectByBrand).use { ps =>
+          ps.stream(brand, 1024).compile.toList
+        }
+      }
+
+      def findById(itemId: ItemId): F[Option[Item]] = postgres.use { session: Session[F] =>
+        session.prepare(selectById).use { ps =>
+          ps.option(itemId)
+        }
+      }
+
+      def create(item: CreateItem): F[ItemId] = postgres.use { session: Session[F] =>
+        session.prepare(createItem).use { cmd =>
+          ID.make[F, ItemId].flatMap { id =>
+            cmd.execute(id ~ item).as(id)
+          }
+        }
+      }
+
+      def update(item: UpdateItem): F[Unit] = postgres.use { session: Session[F] =>
+        session.prepare(updateItem).use { cmd =>
+          cmd.execute(item).void
+        }
+      }
+    }
+}
+
+private object ItemSQL {
+
+  val decoder: Decoder[Item] =
+    (itemId ~ itemName ~ itemDesc ~ money ~ brandId ~ brandName ~ categoryId ~ categoryName).map {
+      case i ~ n ~ d ~ p ~ bi ~ bn ~ ci ~ cn =>
+        Item(i, n, d, p, Brand(bi, bn), Category(ci, cn))
+    }
+
+  val selectAll: Query[Void, Item] =
+    sql"""
+      SELECT i.uuid, i.name, i.description, i.price,
+             b.uuid, b.name, c.uuid, c.name
+      FROM items AS i
+      INNER JOIN brands AS b ON i.brand_id = b.uuid
+      INNER JOIN categories AS c ON i.category_id = c.uuid
+      
+    """.query(decoder)
+
+  val selectByBrand: Query[BrandName, Item] =
+    sql"""
+      SELECT i.uuid, i.name, i.description, i.price,
+             b.uuid, b.name, c.uuid, c.name
+      FROM items AS i
+      INNER JOIN brands AS b ON i.brand_id = b.uuid
+      INNER JOIN categories AS c ON i.category_id = c.uuid
+      WHERE b.name LIKE $brandName
+    """.query(decoder)
+
+  val selectById: Query[ItemId, Item] =
+    sql"""
+      SELECT i.uuid, i.name, i.description, i.price,
+             b.uuid, b.name, c.uuid, c.name
+      FROM items AS i
+      INNER JOIN brands AS b ON i.brand_id = b.uuid
+      INNER JOIN categories AS c ON i.category_id = c.uuid
+      WHERE i.uuid = $itemId
+    """.query(decoder)
+
+  val createItem: Command[ItemId ~ CreateItem] =
+    sql"""
+      INSERT INTO items
+      VALUES (
+        $itemId, $itemName, $itemDesc,
+        $money, $brandId, $categoryId
+      )
+    """.command.contramap {
+      case id ~ i =>
+        id ~ i.name ~ i.description ~ i.price ~ i.brandId ~ i.categoryId
+    }
+
+  val updateItem: Command[UpdateItem] =
+    sql"""
+      UPDATE items
+      SET price = $money
+      WHERE uuid = ${itemId}
+    """.command.contramap { i =>
+      i.price ~ i.id
+    }
+
 }
--- modules/core/src/main/scala/shop/sql/codecs.scala
+++ modules/core/src/main/scala/shop/sql/codecs.scala
@@ -2,14 +2,22 @@ package shop.sql
 
 import shop.domain.brand._
 import shop.domain.category._
-
+import shop.domain.item._
 
 import skunk.Codec
 import skunk.codec.all._
+import squants.market._
 
 object codecs {
-    val brandId: Codec[BrandId] = uuid.imap[BrandId](BrandId(_))(_.value)
-    val brandName: Codec[BrandName] = varchar.imap[BrandName](BrandName(_))(_.value)
-    val categoryId: Codec[CategoryId] = uuid.imap[CategoryId](CategoryId(_))(_.value)
-    val categoryName: Codec[CategoryName] = varchar.imap[CategoryName](CategoryName(_))(_.value)
+  val brandId: Codec[BrandId]     = uuid.imap[BrandId](BrandId(_))(_.value)
+  val brandName: Codec[BrandName] = varchar.imap[BrandName](BrandName(_))(_.value)
+
+  val categoryId: Codec[CategoryId]     = uuid.imap[CategoryId](CategoryId(_))(_.value)
+  val categoryName: Codec[CategoryName] = varchar.imap[CategoryName](CategoryName(_))(_.value)
+
+  val itemId: Codec[ItemId]            = uuid.imap[ItemId](ItemId(_))(_.value)
+  val itemName: Codec[ItemName]        = varchar.imap[ItemName](ItemName(_))(_.value)
+  val itemDesc: Codec[ItemDescription] = varchar.imap[ItemDescription](ItemDescription(_))(_.value)
+
+  val money: Codec[Money] = numeric.imap[Money](USD(_))(_.amount)
 }
