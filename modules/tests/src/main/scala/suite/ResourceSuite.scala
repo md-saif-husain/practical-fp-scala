546e09ddd3429cfe1cad869cdc539b027aab5b06
Md Saif Husain <mdhusain02@gmail.com>
2023-10-07 11:27:33 +0530

    test: helper for Resource

---
+++ modules/tests/src/main/scala/suite/ResourceSuite.scala
@@ -0,0 +1,45 @@
+package suite
+
+import weaver.IOSuite
+import weaver.scalacheck.Checkers
+import weaver.scalacheck.CheckConfig
+import cats.effect.Resource
+import cats.effect.IO
+import cats.syntax.flatMap._
+import weaver.Expectations
+
+abstract class ResourceSuite extends IOSuite with Checkers {
+  override def checkConfig: CheckConfig = CheckConfig.default.copy(minimumSuccessful = 1)
+
+  implicit class SharedResOps(res: Resource[IO, Res]) {
+    def beforeAll(f: Res => IO[Unit]): Resource[IO, Res] =
+      res.evalTap(f)
+
+    def afterAll(f: Res => IO[Unit]): Resource[IO, Res] =
+      res.flatTap(x => Resource.make(IO.unit)(_ => f(x)))
+
+    def testBeforeAfterEach(
+        after: Res => IO[Unit],
+        before: Res => IO[Unit]
+    ): String => (Res => IO[Expectations]) => Unit =
+      name =>
+        fa =>
+          test(name) { res =>
+            before(res) >> fa(res).guarantee(after(res))
+          }
+
+    def testAfterEach(
+        after: Res => IO[Unit]
+    ): String => (Res => IO[Expectations]) => Unit = 
+        testBeforeAfterEach(_ => IO.unit, after)
+
+    def testBeforeEach(
+        before: Res => IO[Unit]
+    ): String => (Res => IO[Expectations]) => Unit = 
+        testBeforeAfterEach(before, _ => IO.unit)
+
+    
+          
+  }
+
+}
